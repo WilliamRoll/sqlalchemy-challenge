2a8c520a8184124eb6f62fc982574e65c3720723
WilliamRoll <broll6595@gmail.com>
2020-11-14 15:07:49 -0500

    app.py file created

---
+++ app.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+#Measurement table
+Measurement =  Base.classes.measurement
+#Station Table
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/tobs<br/>"
+    )
+
+@app.route("/api/v1.0/names")
+def names():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all passenger names"""
+    # Query all passengers
+    results = session.query(Passenger.name).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+
+    return jsonify(all_names)
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+        # Query all passengers
+        prcp = [Measurement.date,
+            func.avg(Measurement.prcp)]
+
+        #12 month of precp query data
+        prcp_twelve_mon = session.query(*prcp).\
+            filter(Measurement.date >= twelve_months).\
+            group_by(Measurement.date).\
+            order_by(Measurement.date).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    prcp_data = []
+    for date, prcp in prcp_twelve_mon:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        prcp_data.append(prcp_dict)
+
+    return jsonify(prcp_data)
